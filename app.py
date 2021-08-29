4dc58c264debf173093228595ed7bdfe82e7cf67
baijuka <baijuka@yahoo.com>
2021-08-29 12:01:03 +0000

    Add user profile

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -57,6 +58,7 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for("profile", username=session["user"]))
             else:
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
@@ -71,6 +73,12 @@ def logout():
     flash("You have been successfully logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
