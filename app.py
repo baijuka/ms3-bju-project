a1a884465596ac7aa19bff6299c2ae8a11759355
baijuka <baijuka@yahoo.com>
2021-08-30 15:48:29 +0000

    Add recipe form input fields

--- app.py
+++ app.py
@@ -86,6 +86,11 @@ def profile(username):
     return render_template("profile.html", user=username, user_recipe=user_recipe)
 
 
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
