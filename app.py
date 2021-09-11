087053bfcfbe26125c9c9093ef4a10389c442583
baijuka <baijuka@yahoo.com>
2021-09-11 13:46:56 +0000

    Add CRUD functionalities for Manage Categories

--- app.py
+++ app.py
@@ -169,7 +169,42 @@ def edit_userInfo(username):
 
 @app.route('/get_categories')
 def get_categories():
-    pass
+    categories = list(mongo.db.categories.find().sort("category_name",1))
+    return render_template("categories.html",categories=categories)
+
+
+@app.route("/add_category", methods=["GET","POST"])
+def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get('category_name')
+        }
+        mongo.db.categories.insert_one(category)    
+        flash("New Category Added")
+        return redirect(url_for('get_categories'))
+
+    return render_template("add_category.html")
+
+
+@app.route("/edit_category/<category_id>", methods=["GET","POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id":ObjectId(category_id)},submit)
+        flash("Category Successfully Updated")
+        return redirect(url_for('get_categories'))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html",category=category)
+
+
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id":ObjectId(category_id)})
+    flash("Category Successfully Deleted")
+    return redirect(url_for('get_categories'))
 
 
 if __name__ == "__main__":
