a79c6c4af9adb2b44267467a234b7b67658c4757
baijuka <baijuka@yahoo.com>
2021-09-12 17:41:41 +0000

    Credits added and edited page content

--- static/js/list_script.js
+++ static/js/list_script.js
@@ -1,23 +1,4 @@
-// var tools_arr=[];
-// var tools_ul=document.getElementById('tools_ul');
-// var btn=document.getElementById('btn_tools');
-// tool_count=0;
-
-// function add_tools(event)
-//     {
-//         event.preventDefault();
-//         tool_count++;
-//         var tools_item = document.getElementById('tools_required').value;
-//         var tools_li = document.createElement('input');
-//         tools_li.setAttribute('name', 'tools');
-//         tools_li.setAttribute('value', tools_item);
-//         tools_ul.append(tools_li);
-//         tools_arr.push(tools_item);
-//         document.getElementById('tools_required').value="";
-//         document.getElementById('tools_required').focus();
-//     };
-
-        
+       
 function add_tools(event)
     {
         event.preventDefault();
